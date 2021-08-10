96355a70d6a81ed335431ac68839a6c7e3093a76
Michael Roxas <work.michaelroxas@gmail.com>
2021-08-10 21:07:44 +0800

    Add components folder/files

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+function Dashboard() {
+  return (
+    <div className="p-20 bg-blue-200">
+      <div className="bg-white p-10 rounded-2xl shadow-lg w-full md:w-1/2 mx-auto">
+        <h2 className="font-bold mb-2 text-xl text-gray-800">Michael Roxas</h2>
+        <p className="text-gray-700">Balance: PHP 1,000,000.00</p>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ src/components/TransactionHistory.js
@@ -0,0 +1,9 @@
+function TransactionHistory() {
+  return (
+    <div className="card">
+      <h3>transaction history</h3>
+    </div>
+  );
+}
+
+export default TransactionHistory;
